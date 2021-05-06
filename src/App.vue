a796dc29282675e4615c8efce67000209f770505
lahoucineABOULHASSAN <em.aboulhassan@gmail.com>
2021-05-06 16:15:02 +0100

    updating css colors

--- src/App.vue
+++ src/App.vue
@@ -8,77 +8,77 @@
 </template>
 
 <script>
-  import Nav from './components/Nav.vue'
-  import Header from './components/Header.vue'
-  import Main from './components/Main.vue'
-  import Footer from './components/Footer.vue'
-  export default {
-    name: 'App',
-    components: {
-      Nav,
-      Header,
-      Main,
-      Footer,
-    },
-  }
+import Nav from "./components/Nav.vue";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Header,
+    Main,
+    Footer,
+  },
+};
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Abel&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=News+Cycle&display=swap');
-  /* VARIABLES DECLARATION */
-  :root {
-    --primary: #f4a950;
-    --secondary: #42b883;
-    --dark: #1d1b1b;
-    --darker: #111111;
-    --light: #e9e9e9;
-    --lighter: #f7f7f7;
-    --lightTransparence: #191818d9;
-    --blue: #3396b5;
-    --bluer: #607d8b;
-    --animate-duration: 2500ms;
-    --animate-delay: 0.9s;
+@import url("https://fonts.googleapis.com/css?family=Abel&display=swap");
+@import url("https://fonts.googleapis.com/css?family=News+Cycle&display=swap");
+/* VARIABLES DECLARATION */
+:root {
+  --primary: #39a275;
+  --secondary: #e2375a;
+  --dark: #1d1b1b;
+  --darker: #111111;
+  --light: #e9e9e9;
+  --lighter: #f7f7f7;
+  --lightTransparence: #191818d9;
+  --blue: #3396b5;
+  --bluer: #607d8b;
+  --animate-duration: 2500ms;
+  --animate-delay: 0.9s;
+}
+* {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+img {
+  width: 100%;
+}
+ul {
+  list-style: none;
+}
+.clear {
+  clear: both;
+}
+.grid {
+  display: grid;
+  grid-gap: 1rem;
+}
+/* -------------------------------------- */
+/* special classes */
+.to-left {
+  float: left;
+}
+.to-right {
+  float: right;
+}
+/* media queries */
+@media screen and (min-width: 1368px) {
+  #root {
+    width: 1350px;
+    margin: 0 auto;
   }
-  * {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    box-sizing: border-box;
+  #root .topnav {
+    width: 1350px;
   }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  img {
-    width: 100%;
-  }
-  ul {
-    list-style: none;
-  }
-  .clear {
-    clear: both;
-  }
-  .grid {
-    display: grid;
-    grid-gap: 1rem;
-  }
-  /* -------------------------------------- */
-  /* special classes */
-  .to-left {
-    float: left;
-  }
-  .to-right {
-    float: right;
-  }
-  /* media queries */
-  @media screen and (min-width: 1368px) {
-    #root {
-      width: 1350px;
-      margin: 0 auto;
-    }
-    #root .topnav {
-      width: 1350px;
-    }
-  }
+}
 </style>
